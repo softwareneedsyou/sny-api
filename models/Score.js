827d904f122bb59d991a25abb8fa7be4fe097f5c
Bastien91580 <bastien91580@yahoo.fr>
2017-07-17 19:31:36 +0200

    Ajout Verification sur models

--- models/Score.js
+++ models/Score.js
@@ -5,6 +5,13 @@ module.exports = function(sequelize) {
     return sequelize.define('Score', {
         score: {
             type: Sequelize.INTEGER,
+            validate: {
+              notNull: true,
+              notEmpty: true,
+              isNumeric: true,
+              isInt: true,
+              min: 0,
+            },
         }
     })
 }
--- models/User.js
+++ models/User.js
@@ -5,24 +5,58 @@ module.exports = function(sequelize) {
     return sequelize.define('User', {
         firstname: {
             type: Sequelize.STRING,
+            validate: {
+              notNull: true,
+              notEmpty: true,
+              len: [3,20],
+              isAlpha: true,
+            },
         },
         lastname: {
             type: Sequelize.STRING,
+            validate: {
+              notNull: true,
+              notEmpty: true,
+              len: [3,20],
+              isAlpha: true,
+            },
         },
         username: {
             type: Sequelize.STRING,
+            validate: {
+              notNull: true,
+              notEmpty: true,
+              len: [3,20],
+            },
         },
         email: {
             type: Sequelize.STRING,
+            validate: {
+              notNull: true,
+              notEmpty: true,
+              isEmail: true,
+            },
         },
         password: {
             type: Sequelize.STRING,
+            validate: {
+              notNull: true,
+              notEmpty: true,
+            },
         },
         token: {
             type: Sequelize.STRING
         },
         admin: {
             type: Sequelize.BOOLEAN,
+            validate: {
+              notNull: true,
+              notEmpty: true,
+              isNumeric: true,
+              isInt: true,
+              min: 0,
+              max : 1,
+            },
         }
     })
 }
